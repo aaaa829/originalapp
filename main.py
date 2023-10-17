84cf1a70fe30976925240bc1f848a56bfbb49016
aaaa <iwaturu829@yahoo.co.jp>
2023-10-17 19:39:27 +0900

    sard

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import cv2
 import tkinter as tk
-import random as rand
+from pygame.locals import *
+import pygame
 
-Xx = 720
-Yy = 480
+rect = {}
+rect2 = {}
 
 
 def click(event):
@@ -14,7 +14,7 @@ def click(event):
     y = event.y
 
     # クリックされた位置に一番近い図形のID取得
-    figure = canvas.find_closest(x, y)
+    figure = canvas1.find_closest(x, y)
 
     # マウスの座標を記憶
     before_x = x
@@ -23,46 +23,76 @@ def click(event):
 
 def drag(event):
     global before_x, before_y
-
     x = event.x
     y = event.y
 
     # 前回からのマウスの移動量の分だけ図形も移動
-    canvas.move(figure, x - before_x, y - before_y)
+    canvas1.move(figure, x - before_x, y - before_y)
     # マウスの座標を記憶
     before_x = x
     before_y = y
 
+    # 座標の中心 canvas1.coords(figure)
+    # 座標の左上隅、右下隅　canvas1.bbox(figure)
+    # canvas1.coords(figure)[0] <= x <= canvas1.coords(figure)[2]
+    # and canvas1.coords(figure)[1] <= y <= canvas1.coords(figure)[3]
+    for i in colors:
+        if canvas1.coords(rect[i]) == canvas1.coords(rect2[i]):
+            canvas1.delete(rect[i], rect2[i])
+            del rect[i]
+            se.play()
+            if len(rect) == 0:
+                quit()
+
+
+def quit():
+    root.quit()
+    root.destroy()
+
 
 def main():
-    global canvas
-
-    app = tk.Tk()
-    app.geometry(f"{Xx}x{Yy}")
+    global root
+    global canvas1
+    w = 1280
+    h = 640
+    root = tk.Tk()
 
     # キャンバス作成
-    canvas = tk.Canvas(app, width=Xx, height=Yy, highlightthickness=0, bg="white")
+    canvas1 = tk.Canvas(root, width=w, height=h, highlightthickness=0, bg="white")
 
-    canvas.grid(row=0, column=0)
+    canvas1.grid(row=0, column=0)
 
     # 色を用意
-    numlist = rand.sample((range(1, 14)), k=10)
-    print(numlist)
-    print(type(numlist))
-    print(len(numlist))
-    imglist = []
-    for n in range(len(numlist)):
-        i = f"originalapp/img/{numlist[n]}.png"
-        img = tk.PhotoImage(file=f"originalapp/img/{numlist[n]}.png").subsample(5)
-        imglist.append(img)
-    print(imglist)
+    global colors
+    colors = (
+        "red",
+        "green",
+        "yellow",
+        "blue",
+        "purple",
+        "pink",
+        "orange",
+        "black",
+        "skyblue",
+    )
 
-    for n in range(len(imglist)):
-        rect = tk.Label(canvas, image=imglist[n])
-        rect.grid(row=0, column=f"{n}")
+    global rect
+    global rect2
+    # 色の数だけ適当に位置をずらしながら長方形（正方形）を描画
+    for i, color in enumerate(colors):
+        rect[color] = canvas1.create_rectangle(
+            i * 60 + 20, 20, i * 60 + 70, 70, fill=color
+        )
+        rect2[color] = canvas1.create_rectangle(
+            (w - (i * 60 + 20)), h - 20, (w - (i * 60 + 70)), h - 70, fill=color
+        )
+        canvas1.tag_bind(rect[color], "<ButtonPress-1>", click)
+        canvas1.tag_bind(rect[color], "<Button1-Motion>", drag)
 
-    app.mainloop()
+    root.mainloop()
 
 
 if __name__ == "__main__":
+    pygame.init()
+    se = pygame.mixer.Sound("originalapp/img/expl.mp3")
     main()
